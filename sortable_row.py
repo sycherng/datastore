0681ff4852708969abff94be66ef1567d5f961fc
scherng <ec2-user@ip-172-31-1-105.us-west-2.compute.internal>
2018-04-04 11:45:59 -0700

    cleaned up sortable_row.py & values.py

--- sortable_row.py
+++ sortable_row.py
@@ -1,34 +1,56 @@
 class SortableRow:
+    """Convenience class to help sort rows 
+    by *arbitrary number of ordering keys when ORDER BY clause has multiple parameters.
+
+    Example: -o STB,DATE,REV:min,VIEW_TIME:max
+
+    *maxes out at 30 ordering keys (6 valid keys * 5 aggregate options)
+    """
+
     def __init__(self, row, order_keys):
-        self.row = row
+        self.attributes_dict = row
         self.order_keys = order_keys
 
-    def compare(self, other, order_keys):
+
+    @staticmethod
+    def _compare(a, b, order_keys):
+        """(SortableRow, SortableRow, list<str>) -> int
+        Compares 2 SortableRow objects 
+        by an arbitrary length list of attributes_dict keys.
+
+        Example order_keys:
+        ['STB', 'DATE', 'REV:min', 'VIEW_TIME:max']
+        """
         for key in order_keys:
-            if self.row[key] > other.row[key]:
+            if a.attributes_dict[key] > b.attributes_dict[key]:
                 return 1
 
         for key in order_keys:
-            if self.row[key] != other.row[key]:
+            if a.attributes_dict[key] != b.attributes_dict[key]:
                 return -1
 
         return 0
 
-    def __lt__(self, other):
-        return self.compare(other, self.order_keys) < 0
 
-    def __le__(self, other):
-        return self.compare(other, self.order_keys) <= 0
+    def __lt__(self, other): #< operator override
+        return SortableRow._compare(self, other, self.order_keys) < 0
 
-    def __gt__(self, other):
-        return self.compare(other, self.order_keys) > 0
 
-    def __ge__(self, other):
-        return self.compare(other, self.order_keys) >= 0
+    def __le__(self, other): #<= operator override
+        return SortableRow._compare(self, other, self.order_keys) <= 0
 
-    def __eq__(self, other):
-        return self.compare(other, self.order_keys) == 0
 
-    def __ne__(self, other):
-        return self.compare(other, self.order_keys) != 0
+    def __gt__(self, other): #> operator override
+        return SortableRow._compare(self, other, self.order_keys) > 0
 
+
+    def __ge__(self, other): #>= operator override
+        return SortableRow._compare(self, other, self.order_keys) >= 0
+
+
+    def __eq__(self, other): #== operator override
+        return SortableRow._compare(self, other, self.order_keys) == 0
+
+
+    def __ne__(self, other): #!= operator override
+        return SortableRow._compare(self, other, self.order_keys) != 0
--- values.py
+++ values.py
@@ -1,6 +1,6 @@
 DATASTORE_HEADING = 'STB|TITLE|DATE|PROVIDER|REV|VIEW_TIME' #sic
 DATASTORE_NAME = 'datastore.txt'
 TEMPFILE_NAME = 'tempstore.txt'
-VALID_KEYS = DATASTORE_HEADING.split("|")
+VALID_KEYS = ['STB', 'TITLE', 'DATE', 'PROVIDER', 'REV', 'VIEW_TIME']
 VALID_FLAGS = ['-f', '-g', '-s', '-o']
 VALID_AGGREGATE_OPTIONS = ['max', 'min', 'sum', 'count', 'collect']
